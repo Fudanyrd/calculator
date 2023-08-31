8b3aa45f4219e55e1cf2cc186d00b48ac0fbb99e
Fudanyrd <143480204+Fudanyrd@users.noreply.github.com>
2023-08-31 20:37:08 +0800

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "calculator\calculator.h"
+#include <cstdlib>
+using std::cout;        using std::ofstream;
+using std::endl;
+
+int main(int argc,char** argv){
+    calculator c1;
+    c1.run();
+    
+    system("pause");
+    return 0;
+}
