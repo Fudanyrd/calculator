460a128558efb30ab5d3da32967c55dc25492a63
Fudanyrd <143480204+Fudanyrd@users.noreply.github.com>
2023-09-02 10:40:41 +0800

    Modify this.mk
    -replace g++ with dollar(CXX)

--- this.mk
+++ this.mk
@@ -1,16 +1,16 @@
 calculator: main.o calculator.o translator.o Expr_node.o complex_num.o
-	g++ -o calculator main.o calculator.o translator.o Expr_node.o complex_num.o
+	$(CXX) -o calculator main.o calculator.o translator.o Expr_node.o complex_num.o
 
 main.o: main.cpp
-	g++ -c main.cpp
+	$(CXX) -c main.cpp
 calculator.o: calculator\calculator.h
-	g++ -c calculator\calculator.cpp
+	$(CXX) -c calculator\calculator.cpp
 translator.o: translator\translator.h
-	g++ -c translator\translator.cpp
+	$(CXX) -c translator\translator.cpp
 Expr_node.o: Expr\Expr_node.h
-	g++ -c Expr\Expr_node.cpp
+	$(CXX) -c Expr\Expr_node.cpp
 complex_num.o: complex_num\complex_num.h
-	g++ -c complex_num\complex_num.cpp
+	$(CXX) -c complex_num\complex_num.cpp
 
 clean:
 	rm main.o calculator.o translator.o Expr_node.o \
